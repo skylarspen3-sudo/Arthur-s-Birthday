9af597532b77094b44f49a49418c497327ff5b7c
dhairya-wotsabot <dhairya.brijesh@wotsabot.com>
2025-08-12 23:10:36 +0530

    a2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 }
+  base: '/Arthur-s-Birthday/', // Replace with your repository name
 });
